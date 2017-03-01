46763b3061101479ab354bb106c7e421bed6e39e
kenshinji <gundam0083ster@gmail.com>
2017-03-01 09:47:16 +0800

    delete unnecessary information

--- src/main/java/com/example/message/Receiver.java
+++ src/main/java/com/example/message/Receiver.java
@@ -4,15 +4,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
-/**
- * <p>Title: Receiver</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2017</p>
- * <p>Company: HP Development Company L.P.</p>
- *
- * @author wangzen
- * @date 2/28/2017
- */
+
 @RabbitListener(queues = "testMQ")
 public class Receiver {
 
